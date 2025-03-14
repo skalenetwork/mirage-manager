29ef165bcf7818d6c62c3c73b93bdd846b535f46
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-03-14 16:51:41 +0200

    Add hardhat project

---
+++ contracts/Nodes.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+
+/**
+ *   Nodes.sol - playa-manager
+ *   Copyright (C) 2025-Present SKALE Labs
+ *   @author Dmytro Stebaiev
+ *
+ *   playa-manager is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published
+ *   by the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   playa-manager is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with playa-manager.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+pragma solidity ^0.8.24;
+
+
+contract Nodes {
+
+}
