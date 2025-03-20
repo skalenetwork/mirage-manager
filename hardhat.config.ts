089508a5d17c66cdefc18ad8f05b1cdc5fcb45c5
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-03-20 18:28:43 +0200

    Fix coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-ethers";
 import "@nomicfoundation/hardhat-network-helpers";
 import '@openzeppelin/hardhat-upgrades';
+import 'solidity-coverage'
 import '@typechain/hardhat';
 
 const config: HardhatUserConfig = {
