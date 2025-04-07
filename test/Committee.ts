d3672239796646561d9296a09b36cce211015b77
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-04-07 18:12:52 +0300

    Add committee selection test

--- test/Committee.ts
+++ test/Committee.ts
@@ -1,5 +1,5 @@
 import chai from "chai";
-import { nodesAreRegisteredAndHeartbeatIsSent } from "./fixtures";
+import { cleanDeployment, nodesAreRegisteredAndHeartbeatIsSent } from "./fixtures";
 import { ethers } from "hardhat";
 import { runDkg } from "./dkg/DKG";
 
@@ -13,7 +13,7 @@ describe("Committee", () => {
             .should.be.revertedWithCustomError(committee, "AccessManagedUnauthorized");
     });
 
-    it("should create committee", async () => {
+    it("should select committee", async () => {
         const {committee, dkg, nodesData} = await nodesAreRegisteredAndHeartbeatIsSent();
         const activeCommitteeIndex = await committee.getActiveCommitteeIndex();
         const nextCommitteeIndex = activeCommitteeIndex + 1n;
@@ -34,5 +34,12 @@ describe("Committee", () => {
         nextCommittee.commonPublicKey.x.b.should.not.be.equal(0n);
         nextCommittee.commonPublicKey.y.a.should.not.be.equal(0n);
         nextCommittee.commonPublicKey.y.b.should.not.be.equal(0n);
+    });
+
+    it("should not allow anyone to set dkg contract", async () => {
+        const [, hacker] = await ethers.getSigners();
+        const {committee} = await cleanDeployment();
+        await committee.connect(hacker).setDkg(hacker)
+            .should.be.revertedWithCustomError(committee, "AccessManagedUnauthorized");
     });
 });
