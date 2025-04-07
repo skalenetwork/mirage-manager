a1c5f2ef915a0616ce3a8bcfb777b9752b5e56b2
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-04-07 18:28:50 +0300

    Add setters tests

--- test/Committee.ts
+++ test/Committee.ts
@@ -1,4 +1,4 @@
-import chai from "chai";
+import chai, { assert } from "chai";
 import { cleanDeployment, nodesAreRegisteredAndHeartbeatIsSent } from "./fixtures";
 import { ethers } from "hardhat";
 import { runDkg } from "./dkg/DKG";
@@ -28,8 +28,11 @@ describe("Committee", () => {
         await runDkg(dkg, nodesData, nextCommittee.dkg);
 
         nextCommittee = await committee.getCommittee(nextCommitteeIndex);
-        nextCommittee.startingTimestamp.should.be.lessThan(2n ** 256n - 1n);
-        nextCommittee.startingTimestamp.should.be.greaterThan((await ethers.provider.getBlock("latest"))?.timestamp);
+        const lastTransactionTimestamp = (await ethers.provider.getBlock("latest"))?.timestamp;
+        assert(lastTransactionTimestamp);
+        nextCommittee.startingTimestamp.should.be.equal(
+            BigInt(lastTransactionTimestamp) + (await committee.transitionDelay())
+        );
         nextCommittee.commonPublicKey.x.a.should.not.be.equal(0n);
         nextCommittee.commonPublicKey.x.b.should.not.be.equal(0n);
         nextCommittee.commonPublicKey.y.a.should.not.be.equal(0n);
@@ -75,5 +78,42 @@ describe("Committee", () => {
         const {committee} = await cleanDeployment();
         await committee.connect(hacker).setTransitionDelay(0xd2n)
             .should.be.revertedWithCustomError(committee, "AccessManagedUnauthorized");
+    });
+
+    it("should set committee size", async () => {
+        const {committee} = await nodesAreRegisteredAndHeartbeatIsSent();
+        const newSize = 13n;
+
+        await committee.setCommitteeSize(newSize);
+
+        (await committee.committeeSize()).should.be.equal(newSize);
+
+        await committee.select();
+
+        const nextCommittee = await committee.getCommittee(await committee.getActiveCommitteeIndex() + 1n);
+        nextCommittee.nodes.length.should.be.equal(newSize);
+    });
+
+    it("should set transition delay", async () => {
+        const {committee, dkg, nodesData} = await nodesAreRegisteredAndHeartbeatIsSent();
+        const activeCommitteeIndex = await committee.getActiveCommitteeIndex();
+        const nextCommitteeIndex = activeCommitteeIndex + 1n;
+        const newTransitionDelay = 0xd2n;
+
+        await committee.setTransitionDelay(newTransitionDelay);
+        await committee.select();
+
+        let nextCommittee = await committee.getCommittee(nextCommitteeIndex);
+
+        await runDkg(dkg, nodesData, nextCommittee.dkg);
+
+        (await committee.transitionDelay()).should.be.equal(newTransitionDelay);
+        nextCommittee = await committee.getCommittee(nextCommitteeIndex);
+        nextCommittee.startingTimestamp.should.be.lessThan(2n ** 256n - 1n);
+        const lastTransactionTimestamp = (await ethers.provider.getBlock("latest"))?.timestamp;
+        assert(lastTransactionTimestamp);
+        nextCommittee.startingTimestamp.should.be.equal(
+            BigInt(lastTransactionTimestamp) + newTransitionDelay
+        );
     });
 });
