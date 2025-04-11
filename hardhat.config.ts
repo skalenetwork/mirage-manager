7e27b3f29bb4e68213ccbaf268ba88216cc42354
Vadim Yavorsky <yavrsky@gmail.com>
2025-04-11 14:08:59 +0100

    Configure Hardhat to use environment variables for network settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,9 +6,24 @@ import "@nomicfoundation/hardhat-chai-matchers";
 import '@openzeppelin/hardhat-upgrades';
 import 'solidity-coverage'
 import '@typechain/hardhat';
+import * as dotenv from "dotenv"
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    hardhat: {
+      accounts: [{
+        balance: "10000000000000000000000",
+        privateKey: process.env.PRIVATE_KEY || ""
+      }]
+    },
+    custom: {
+      url: process.env.ENDPOINT || "http://localhost:8545",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    }
+  }
 };
 
 export default config;
