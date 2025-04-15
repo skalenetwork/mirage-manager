80f39b4a81bd9f4167c7da435720166b3a8f3110
Vadim Yavorsky <yavrsky@gmail.com>
2025-04-15 18:11:16 +0100

    Remove hardhat network accounts configuration from hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -13,12 +13,6 @@ dotenv.config();
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
-    hardhat: {
-      accounts: [{
-        balance: "10000000000000000000000",
-        privateKey: process.env.PRIVATE_KEY || ""
-      }]
-    },
     custom: {
       url: process.env.ENDPOINT || "http://localhost:8545",
       accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
