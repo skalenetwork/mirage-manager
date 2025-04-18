3d7a7ede06311adaf1d78cde611809719d63ca02
Vadim Yavorsky <yavrsky@gmail.com>
2025-04-18 14:18:06 +0100

    Add INFURA_API_TOKEN support for dynamic MAINNET_ENDPOINT in deployment scripts

--- scripts/test_deploy.sh
+++ scripts/test_deploy.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
-
-export MAINNET_ENDPOINT="https://eth.llamarpc.com"
+if [ -n "$INFURA_API_TOKEN" ]; then
+    export MAINNET_ENDPOINT="https://mainnet.infura.io/v3/${INFURA_API_TOKEN}"
+else
+    export MAINNET_ENDPOINT="https://eth.llamarpc.com"
+fi
 export CHAIN_HASH="0x000003d28774d2845ee8b9f656cc77328199a7a69b03ce2b9578f230be679c9f"
 export TARGET="production"
 
