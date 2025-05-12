541e8597023e0360986cdadad2bf36c63acdf58a
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2025-05-12 11:02:14 +0100

    fix cspell word: hexlify

--- test/tools/signatures.ts
+++ test/tools/signatures.ts
@@ -3,6 +3,8 @@ import {ec} from "elliptic";
 
 const secp256k1EC = new ec("secp256k1");
 
+// cspell:words hexlify
+
 export function getPublicKey(wallet: BaseWallet): [BytesLike, BytesLike] {
     const publicKey = secp256k1EC.keyFromPrivate(wallet.privateKey.slice(2)).getPublic();
     const pubA = ethers.zeroPadValue(ethers.hexlify(publicKey.getX().toBuffer()), 32);
