0b50b7e8178d9c11b973d63f5aab69b8d17d5a44
Vadim Yavorsky <yavrsky@gmail.com>
2025-07-24 17:05:48 +0100

    Add test for retrieving from a committee node in Staking.ts

--- test/Staking.ts
+++ test/Staking.ts
@@ -1,5 +1,5 @@
 import chai, { assert, expect } from "chai";
-import { registeredOnlyNodes } from "./tools/fixtures";
+import { registeredOnlyNodes, stakedNodes } from "./tools/fixtures";
 import { ethers } from "hardhat";
 import { zip } from "lodash";
 
@@ -250,6 +250,25 @@ describe("Staking", () => {
             .should.be.equal(0n);
         (await staking.getStakedAmountFor(user))
             .should.be.equal(amount2 + node2Reward + roundingError);
+    });
+
+    it("should allow to retrieve from a node from committee", async () => {
+        const {committee, staking} = await stakedNodes();
+        const [, user] = await ethers.getSigners();
+        const activeCommittee = await committee.getCommittee(await committee.getActiveCommitteeIndex());
+        const amount = ethers.parseEther("1");
+        const retrieveAmount = ethers.parseEther("0.5");
+
+        // stake to a committee node
+        const committeeNode = activeCommittee.nodes[0];
+        await staking.connect(user).stake(committeeNode, {value: amount});
+
+        // should allow retrieval from committee node
+        await staking.connect(user).retrieve(committeeNode, retrieveAmount)
+            .should.changeEtherBalance(user, retrieveAmount);
+
+        (await staking.connect(user).getStakedAmount())
+            .should.be.equal(amount - retrieveAmount);
     });
 
     it("should not pay rewards to stakers of unhealthy nodes", async () => {
