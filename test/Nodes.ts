fcc0399bca740b17abc1c17cf462adb547d4cfad
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-08-04 19:38:57 +0300

    Update timeout

--- test/Nodes.ts
+++ test/Nodes.ts
@@ -483,7 +483,10 @@ describe("Nodes", function () {
 
     });
 
-    it("should should not allow deleting node if node in current or next committee or has stake", async () => {
+    it("should should not allow deleting node if node in current or next committee or has stake", async function () {
+        // TODO: this test is taking too long only on old versions of nodejs
+        // remove custom timeout after deprecation of nodejs 18
+        this.timeout(60000); // 1 minutes
         const {committee, nodesData, nodes, status, staking} = await whitelistedAndStakedNodes();
         await committee.setCommitteeSize(5); // to save resources
         await sendHeartbeat(status, nodesData.slice(0, 10)); // to save time
