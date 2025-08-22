3803dc7d98f58aff529d9885625546f62ca5340e
Dmytro Stebaiev <dimastebaev@gmail.com>
2025-08-22 18:11:41 +0300

    Add natspec to the FieldOperationsTester

--- contracts/test/utils/FieldOperationsTester.sol
+++ contracts/test/utils/FieldOperationsTester.sol
@@ -27,18 +27,32 @@ import { Fp2Operations } from "../../utils/fieldOperations/Fp2Operations.sol";
 import { G2Operations } from "../../utils/fieldOperations/G2Operations.sol";
 
 
+/// @title IFiedOperationsTester - interface for testing field operations
+/// @author Dmytro Stebaiev
+/// @notice This interface is used for testing field operations
 interface IFiedOperationsTester {
+    /// @notice Adds two G2 points
+    /// @param value1 The first G2 point
+    /// @param value2 The second G2 point
+    /// @return result The resulting G2 point after addition
     function addG2(IDkg.G2Point calldata value1, IDkg.G2Point calldata value2)
         external
         view
         returns (IDkg.G2Point memory result);
 }
 
+/// @title FieldOperationsTester - contract for testing field operations
+/// @author Dmytro Stebaiev
+/// @notice This contract is used for testing field operations
 contract FieldOperationsTester is IFiedOperationsTester{
 
     using Fp2Operations for IDkg.Fp2Point;
     using G2Operations for IDkg.G2Point;
 
+    /// @notice Adds two G2 points
+    /// @param value1 The first G2 point
+    /// @param value2 The second G2 point
+    /// @return result The resulting G2 point after addition
     function addG2(IDkg.G2Point calldata value1, IDkg.G2Point calldata value2)
         external
         view
