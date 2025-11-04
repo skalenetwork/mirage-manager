5a739ae74bc09eaeb02166c7e5da32a77f2703a8
Eduardo Vasques <eduardo.vasques@skalelabs.com>
2025-11-04 13:31:11 +0000

    add NatSpec to foundry tests and update dictionary terms

--- test-foundry/handlers/RBTHandler.sol
+++ test-foundry/handlers/RBTHandler.sol
@@ -21,40 +21,107 @@
 
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
 import {RedBlackTreeTester, NodeId} from "../../contracts/test/structs/RedBlackTreeTester.sol";
 
+import {Test} from "../../lib/forge-std/src/Test.sol";
 
-contract RBTHandler is Test {
-    RedBlackTreeTester rbt;
-    mapping(NodeId => bool) public insertedKeys;
+/**
+ * @title IRBTHandler
+ * @author Eduardo Vasques
+ * @notice Interface of the Red-Black Tree Tester handler
+ */
+interface IRBTHandler {
+    /**
+     * @notice Inserts a node with a given weight into the Red-Black Tree
+     * @param nodeIndex Index of node in the fixture node array
+     * @param weight The weight to associate with the node (must be greater than 0)
+     */
+    function insert(uint8 nodeIndex, uint48 weight) external;
+
+    /**
+     * @notice Removes a node from the Red-Black Tree
+     * @param nodeIndex Index of node in the fixture node array
+     */
+    function remove(uint8 nodeIndex) external;
+
+    /**
+     * @notice Updates the weight of an existing node in the Red-Black Tree
+     * @param nodeIndex Index of node in the fixture node array
+     * @param weight The new weight for the node (must be greater than 0)
+     */
+    function setWeight(uint8 nodeIndex, uint48 weight) external;
+
+    /**
+     * @notice Checks if a node has been inserted into the tree
+     * @param node The NodeId to check
+     * @return inserted True if the node is in the tree, false otherwise
+     */
+    function insertedKeys(NodeId node) external view returns (bool inserted);
+
+    /**
+     * @notice Gets a key from the key list by index
+     * @param index The index in the key list
+     * @return nodeId The NodeId at the specified index
+     */
+    function keyList(uint256 index) external view returns (NodeId nodeId);
+
+    /**
+     * @notice Gets a fixture node by index
+     * @param index The index of the node in the fixture array
+     * @return nodeId The NodeId at the specified index
+     */
+    function fixtureNode(uint256 index) external view returns (NodeId nodeId);
+}
+
+/**
+ * @title RBT Handler
+ * @author Eduardo Vasques
+ * @notice Handler contract for the Red-Black Tree Test contract
+ */
+contract RBTHandler is Test, IRBTHandler {
+
+    /// @notice The Red-Black Tree tester contract instance
+    RedBlackTreeTester public rbt;
+
+    /// @inheritdoc IRBTHandler
+    mapping(NodeId node => bool inserted) public insertedKeys;
+
+    /// @inheritdoc IRBTHandler
     NodeId[] public keyList;
 
+    /// @inheritdoc IRBTHandler
     NodeId[] public fixtureNode;
 
+    /**
+     * @notice Constructor
+     * @param _rbt The address of the RedBlackTreeTester contract
+     * @dev Max is set to 150 nodes in the tree - corresponds to a Max tree height of 8
+     * @dev If increased beyond 256 nodes, adjust nodeIndex inputs to allow indexes over 255
+     */
     constructor(RedBlackTreeTester _rbt) {
         rbt = _rbt;
-
-        // 150 nodes corresponds to a tree height of 8
-        for(uint256 i = 1; i <= 150; ++i){
+        for(uint256 i = 1; i < 151; ++i){
             fixtureNode.push(NodeId.wrap(i));
         }
     }
 
-    function insert(uint8 nodeIndex, uint48 weight) public {
+    /// @inheritdoc IRBTHandler
+    function insert(uint8 nodeIndex, uint48 weight) public override {
         NodeId node = fixtureNode[nodeIndex % fixtureNode.length];
         vm.assume(!insertedKeys[NodeId.wrap(NodeId.unwrap(node) - 1)]);
         vm.assume(weight > 0);
         _insert(node, weight);
     }
 
-    function remove(uint8 nodeIndex) external {
+    /// @inheritdoc IRBTHandler
+    function remove(uint8 nodeIndex) public override {
         NodeId node = fixtureNode[nodeIndex % fixtureNode.length];
         vm.assume(insertedKeys[NodeId.wrap(NodeId.unwrap(node) - 1)]);
         _remove(node);
     }
 
-    function setWeight(uint8 nodeIndex, uint48 weight) external {
+    /// @inheritdoc IRBTHandler
+    function setWeight(uint8 nodeIndex, uint48 weight) public override {
         NodeId node = fixtureNode[nodeIndex % fixtureNode.length];
         vm.assume(insertedKeys[NodeId.wrap(NodeId.unwrap(node) - 1)]);
         vm.assume(weight > 0);
@@ -62,12 +129,21 @@ contract RBTHandler is Test {
         rbt.setWeight(node, weight);
     }
 
+    /**
+     * @notice Internal helper to insert a node into the tree
+     * @param node The NodeId to insert
+     * @param weight The weight to associate with the node
+     */
     function _insert(NodeId node, uint256 weight) private {
         rbt.insertSmallest(node, weight);
         insertedKeys[NodeId.wrap(NodeId.unwrap(node) - 1)] = true;
         keyList.push(node);
     }
 
+    /**
+     * @notice Internal helper to remove a node from the tree
+     * @param node The NodeId to remove
+     */
     function _remove(NodeId node) private {
         rbt.remove(node);
 
--- test-foundry/handlers/StatusHandler.sol
+++ test-foundry/handlers/StatusHandler.sol
@@ -22,23 +22,93 @@
 
 pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
-
 import {Duration, NodeId, Status} from "../../contracts/Status.sol";
-import {Fair} from "../../contracts/utils/Fund.sol";
 
-contract StatusHandler is Test {
+import {Test} from "../../lib/forge-std/src/Test.sol";
 
+/**
+ * @title IStatusHandler
+ * @author Eduardo Vasques
+ * @notice Interface for the Status contract testing handler
+ */
+interface IStatusHandler {
+    /**
+     * @notice Whitelists a node from the fixtureNode list
+     * @param nodeIndex Index into the fixture node array (modulo array length)
+     */
+    function whitelistNode(uint8 nodeIndex) external;
+
+    /**
+     * @notice Removes a node from the whitelist
+     * @param nodeIndex Index into the fixture node array (modulo array length)
+     */
+    function removeNodeFromWhitelist(uint8 nodeIndex) external;
+
+    /**
+     * @notice Sends alive heartbeats from a random sample of nodes
+     * @param numAlives Number of random nodes to send heartbeats (modulo fixture length)
+     */
+    function randomAlives(uint8 numAlives) external;
+
+    /**
+     * @notice Warps time to eject all unhealthy nodes, then sends heartbeats from a sample
+     * @param numAlives Number of random nodes to send heartbeats after ejection
+     */
+    function alivesSomeWithAllEjected(uint256 numAlives) external;
+
+    /**
+     * @notice Warps time to make all nodes unhealthy, then sends heartbeats from all
+     */
+    function alivesAfterAllUnhealthy() external;
+
+    /**
+     * @notice Gets the Status contract instance
+     * @return status The Status contract being tested
+     */
+    function status() external view returns (Status status);
+
+    /**
+     * @notice Gets a one of the registered nodes during deployment by index
+     * @param index The index of the node in the fixture array
+     * @return nodeId The NodeId at the specified index
+     */
+    function fixtureNode(uint256 index) external view returns (NodeId nodeId);
+
+    /**
+     * @notice Gets the admin address
+     * @return admin The address with admin privileges
+     */
+    function admin() external view returns (address admin);
+}
+/**
+ * @title Status Handler
+ * @author Eduardo Vasques
+ * @notice Handler contract for testing the Status contract
+ */
+contract StatusHandler is Test, IStatusHandler {
+    /// @inheritdoc IStatusHandler
     Status public status;
+
+    /// @inheritdoc IStatusHandler
     NodeId[] public fixtureNode;
+
+    /// @inheritdoc IStatusHandler
     address public admin;
 
+    error InvalidSamplingRequest();
+    error StatusAddressNotSet();
+
+    /**
+     * @notice Constructor
+     * @param _status The Status contract address
+     * @param _admin The admin address
+     */
     constructor(address _status, address _admin) {
         status = Status(_status);
         admin = _admin;
-        require(address(status) != address(0), "STATUS not set");
+        require(address(_status) != address(0), StatusAddressNotSet());
         // scan first 256 active nodes
-        for (uint256 i = 1; i < 257; i++) {
+        for (uint256 i = 1; i < 257; ++i) {
             NodeId node = NodeId.wrap(i);
             if (status.nodes().activeNodeExists(node)) {
                 fixtureNode.push(node);
@@ -46,7 +116,8 @@ contract StatusHandler is Test {
         }
     }
 
-    function whitelistNode(uint8 nodeIndex) public {
+    /// @inheritdoc IStatusHandler
+    function whitelistNode(uint8 nodeIndex) public override {
         NodeId node = fixtureNode[nodeIndex % fixtureNode.length];
         vm.assume(!status.isWhitelisted(node));
         address nodeOwner = status.nodes().getNode(node).nodeAddress;
@@ -56,7 +127,8 @@ contract StatusHandler is Test {
         assert(status.isWhitelisted(node));
     }
 
-    function blacklistNode(uint8 nodeIndex) public {
+    /// @inheritdoc IStatusHandler
+    function removeNodeFromWhitelist(uint8 nodeIndex) public override {
         NodeId node = fixtureNode[nodeIndex % fixtureNode.length];
         vm.assume(status.isWhitelisted(node));
         address nodeOwner = status.nodes().getNode(node).nodeAddress;
@@ -66,10 +138,11 @@ contract StatusHandler is Test {
         assert(!status.isWhitelisted(node));
     }
 
-    function randomAlives(uint8 numAlives) public {
+    /// @inheritdoc IStatusHandler
+    function randomAlives(uint8 numAlives) public override {
         uint256 alives = uint256(numAlives) % fixtureNode.length;
         uint256[] memory nodes = _getRandomSample(alives, fixtureNode.length);
-        for (uint256 i = 0; i < alives; i++) {
+        for (uint256 i = 0; i < alives; ++i) {
             address nodeOwner = status.nodes().getNode(NodeId.wrap(nodes[i])).nodeAddress;
             assert(nodeOwner != address(0));
             vm.prank(nodeOwner);
@@ -77,16 +150,19 @@ contract StatusHandler is Test {
         }
     }
 
-    function alivesSomeWithAllEjected(uint256 numAlives) public {
-        uint256 alives = uint256(numAlives) % fixtureNode.length;
-        uint256[] memory nodes = _getRandomSample(alives, fixtureNode.length);
+    /// @inheritdoc IStatusHandler
+    function alivesSomeWithAllEjected(uint8 numAlives) public override {
+        uint256 numNodes = fixtureNode.length;
+
+        uint256 alives = uint256(numAlives) % numNodes;
+        uint256[] memory nodes = _getRandomSample(alives, numNodes);
 
         vm.warp(block.timestamp + 2 * Duration.unwrap(status.heartbeatInterval()));
 
-        for (uint256 i = 0; i < fixtureNode.length; i++) {
+        for (uint256 i = 0; i < numNodes; ++i) {
             status.committee().ejectUnhealthyNode();
         }
-        for (uint256 i = 0; i < alives; i++) {
+        for (uint256 i = 0; i < alives; ++i) {
             address nodeOwner = status.nodes().getNode(NodeId.wrap(nodes[i])).nodeAddress;
             assert(nodeOwner != address(0));
             vm.prank(nodeOwner);
@@ -94,9 +170,11 @@ contract StatusHandler is Test {
         }
     }
 
-    function alivesAfterAllUnhealthy() public {
+    /// @inheritdoc IStatusHandler
+    function alivesAfterAllUnhealthy() public override {
         vm.warp(block.timestamp + 2 * Duration.unwrap(status.heartbeatInterval()));
-        for (uint256 i = 0; i < fixtureNode.length; i++) {
+        uint256 numNodes = fixtureNode.length;
+        for (uint256 i = 0; i < numNodes; ++i) {
             address nodeOwner = status.nodes().getNode(fixtureNode[i]).nodeAddress;
             assert(nodeOwner != address(0));
             vm.prank(nodeOwner);
@@ -106,34 +184,41 @@ contract StatusHandler is Test {
 
     // Helpers
 
+    /**
+     * @notice Gets a pseudo-random sample of size `x` from the first `n` elements of fixtureNode
+     * @param x Size of the random sample pretended
+     * @param n Size of the original array
+     * @return sample The random sample array
+     * @dev `n` intentionally allows to control the subset of fixtureNode being sampled from
+     */
     function _getRandomSample(
         uint256 x,
         uint256 n
     )
         private
         view
-        returns (uint256[] memory)
+        returns (uint256[] memory sample)
     {
         // Handle edge cases
         if (x == 0) {
             return new uint256[](0);
         }
-        if (x > n) {
-            revert("Sample size cannot be larger than the array length.");
-        }
+
+        // solhint-disable-next-line gas-strict-inequalities
+        require(x <= n && n <= fixtureNode.length, InvalidSamplingRequest());
 
         // Create a copy to avoid modifying the original array
         uint256[] memory arr = new uint256[](n);
-        for (uint256 i = 0; i < n; i++) {
+        for (uint256 i = 0; i < n; ++i) {
             arr[i] = NodeId.unwrap(fixtureNode[i]);
         }
 
         uint256[] memory arrCopy = new uint256[](n);
-        for (uint256 i = 0; i < n; i++) {
+        for (uint256 i = 0; i < n; ++i) {
             arrCopy[i] = arr[i];
         }
 
-        for (uint256 i = 0; i < x; i++) {
+        for (uint256 i = 0; i < x; ++i) {
             // Generate a random index from the unshuffled part of the array
             uint256 j = i + (uint256(keccak256(abi.encodePacked(block.timestamp, i))) % (n - i));
 
@@ -145,7 +230,7 @@ contract StatusHandler is Test {
 
         // Create the result array and copy the first X elements from the shuffled copy
         uint256[] memory result = new uint256[](x);
-        for (uint256 i = 0; i < x; i++) {
+        for (uint256 i = 0; i < x; ++i) {
             result[i] = arrCopy[i];
         }
 
